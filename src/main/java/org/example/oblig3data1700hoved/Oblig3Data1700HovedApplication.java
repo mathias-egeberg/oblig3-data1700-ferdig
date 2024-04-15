2a6290e983df59c52d0d030335cf4cc2beda1b98
Mathais <mathias.egeberg@hotmail.no>
2024-04-15 09:35:22 +0200

    commit1

---
+++ src/main/java/org/example/oblig3data1700hoved/Oblig3Data1700HovedApplication.java
@@ -0,0 +1,13 @@
+package org.example.oblig3data1700hoved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig3Data1700HovedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig3Data1700HovedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/oblig3data1700hoved/Oblig3Data1700HovedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.oblig3data1700hoved;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig3Data1700HovedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
